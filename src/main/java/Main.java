165a303c688c2c47797d303365544a4fd1147187
Abdella <abdella.shukur2@gmail.com>
2022-07-20 14:49:09 -0400

    most of code is working

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,15 +1,15 @@
 public class Main {
     public static void main(String[] args) {
-        ConcatStringFunction func = str -> String.join("", str).toUpperCase();
-        String res = func.apply("banana");
+        ConcatStringFunction func = (str1,str2, str3) -> String.join( "",str1, str2, str3).toUpperCase();
+        String res = func.apply("banana", "grape", "Smoothie");
         System.out.println(res); // BANANAGRAPESMOOTHIE
 
-				String res2 = func.apply("apple");
+				String res2 = func.apply("apple","orange", "pineapple");
 				System.out.println(res2); // APPLEORANGEPINEAPPLESMOOTHIE
     }
 }
 
 @FunctionalInterface
 interface ConcatStringFunction {
-    String apply(String str2);
+    String apply(String str1, String str2, String str3);
 };
